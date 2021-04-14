ce38d94420162b22618ba143bd60e2886366565e
Benjamin Turpin <lhp6.bturpin@gmail.com>
2021-04-14 15:51:01 +0200

    Mise en ligne

---
+++ assets/js/scrollspy.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", function(){
+  window.addEventListener('scroll', function() {
+      if (window.scrollY > 480) {
+        document.getElementById('navbar_top').classList.add('fixed-top');
+        // add padding top to show content behind navbar
+        navbar_height = document.querySelector('.navbar').offsetHeight;
+        document.body.style.paddingTop = navbar_height + 'px';
+      } else {
+        document.getElementById('navbar_top').classList.remove('fixed-top');
+         // remove padding top from body
+        document.body.style.paddingTop = '0';
+      } 
+  });
+});
+
+var scrollSpy = new bootstrap.ScrollSpy(document.body, {
+  target: '#navbar_top'
+})
+
+var dataSpyList = [].slice.call(document.querySelectorAll('[data-bs-spy="scroll"]'))
+dataSpyList.forEach(function (dataSpyEl) {
+  bootstrap.ScrollSpy.getInstance(dataSpyEl)
+    .refresh()
+})
